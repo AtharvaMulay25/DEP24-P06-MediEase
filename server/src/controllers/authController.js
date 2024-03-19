061a3136b7d76d61f3813258108239585f5bb0d1
harshit kumar ravi <hkravi2002@gmail.com>
2024-03-19 18:12:30 +0530

    Implemented Cookies functionality to send jwt

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,3 +1,5 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
 const { v4: uuidv4 } = require("uuid");
 const ExpressError = require("../utils/ExpressError");
 const { generateToken, verifyToken } = require("../utils/handleJWT.js");
---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,51 @@
+//utils
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
+const { verifyToken } = require("../utils/handleJWT");
+const ExpressError = require('../utils/ExpressError');
+
+const authMiddleware = (role) => {
+    return async (req, res, next) => {
+        try {
+            const token = req.cookies.token;
+
+            if (!token) {
+                const error = new ExpressError("Token not found", 401);
+                next(error);
+            }
+
+            const data = verifyToken(token);
+
+            if (!data) {
+                const error = new ExpressError(
+                    "Request is not authorized",
+                    401
+                );
+                next(error);
+            }
+
+            const { email } = data;
+            req.user = await prisma.user.findUnique({
+                where: {
+                    email,
+                },
+            });
+            req.recievedToken = token;
+
+            if (!req.user || (req.user.role !== role)) {
+                const error = new ExpressError(
+                    "User does not exist",
+                    404
+                );
+                next(error);
+            } else {
+                next();
+            }
+        } catch (err) {
+            const error = new ExpressError(err.message, 401);
+            next(error);
+        }
+    };
+}
+
+module.exports = authMiddleware;
