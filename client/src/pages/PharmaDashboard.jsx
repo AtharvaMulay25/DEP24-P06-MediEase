541543abbab27f9d2d78009d2222af27a9983861
Harshit Kumar Ravi <hkravi2002@gmail.com>
2024-03-30 17:00:42 +0530

    Integrated Doctor dashboard with backend

--- client/src/pages/PharmaDashboard.jsx
+++ client/src/pages/PharmaDashboard.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../index.css";
-import { useState, useEffect } from "react";
 import Chart from "react-apexcharts";
 import {
   Card,
