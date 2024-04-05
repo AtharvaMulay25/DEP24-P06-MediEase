e9bf89f6fa926fe315c11f7b563735e106d8e9d5
Atharva Suhas Mulay <atharvamulay25@gmail.com>
2024-04-05 14:35:25 +0530

    Update: About section of Landing Page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,34 +1,24 @@
 import React from "react";
 import img from "../assets/img/about.jpg";
-
+import "../styles/About.css";
 const About = () => {
+  const tagline = "Healthcare reimagined for you...";
   return (
-    <div className=" min-h-screen flex flex-col lg:flex-row justify-evenly lg:justify-between items-center lg:px-32 px-5 pt-24 lg:pt-16 gap-5 bg-blue-50">
-      <div className=" w-full lg:w-3/4 space-y-4">
-        <h1 className=" text-4xl font-semibold text-center lg:text-start">
-          About Us
-        </h1>
-        <p className=" text-justify lg:text-start">
-          At our institute hospital, we are committed to providing exceptional
-          healthcare services tailored to your needs. With a dedicated team of
-          professionals, we prioritize patient well-being above all else. Our
-          goal is to deliver comprehensive medical care with compassion and
-          efficiency, ensuring your health and satisfaction.
-        </p>
+    <div className="min-h-screen flex flex-col lg:flex-row justify-evenly lg:justify-between items-center lg:px-32 px-5 pt-24 lg:pt-16 gap-5 bg-blue-50">
+      <div className="w-full lg:w-3/4 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-semibold text-center lg:text-start text-typing">
+          <p>{tagline}</p>
+        </h2>
         <p className="text-justify lg:text-start">
-          Our patient-centric approach ensures that individuals have easy access
-          to their medical history, prescriptions, and studnets have the ability
-          to apply for medical leave using our page, with the medical history we
-          provided. conveniently. On the staff side, our doctors benefit from
-          streamlined scheduling processes and access to patient records,
-          facilitating efficient and personalized care. Meanwhile, our
-          paramedical staff can effectively manage medical stock, update
-          inventory, and oversee supplier relations, ensuring seamless
-          operations.
+          Exceptional healthcare tailored to you. Prioritizing your well-being
+          with compassion and efficiency. Accessible medical history,
+          prescriptions, and leave applications. Streamlined scheduling and
+          personalized care for doctors. Efficient medical stock management for
+          seamless operations.
         </p>
       </div>
-      <div className=" w-full lg:w-3/4">
-        <img className=" rounded-lg" src={img} alt="img" />
+      <div className="w-full lg:w-3/4">
+        <img className="rounded-lg" src={img} alt="img" />
       </div>
     </div>
   );
