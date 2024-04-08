f25318aa2d02d7eb801b1e47beb8254a61d2629b
Harshit Kumar Ravi <hkravi2002@gmail.com>
2024-04-08 23:08:05 +0530

    Fixes cors error

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 //TODO: Change the origin to deployed frontend URL
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true
 }));
 
