7873e8acd60e3507aed4a25d857d2cca956a7f44
Aryaman Gupta <aryamang2002@gmail.com>
2024-04-17 03:14:53 +0530

    [Fix] Doctor Dashboard

--- client/src/pages/DoctorDashboard.jsx
+++ client/src/pages/DoctorDashboard.jsx
@@ -263,17 +263,17 @@ const DoctorDashboard = () => {
               <div className="p-6">
                 <h5 className="flex items-center mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
                   <FaUserDoctor className="h-7 w-7 mr-2" />
-                  Doctor Details
+                  Staff Details
                 </h5>
                 <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                  All the doctors present in campus.
+                  All staff present in campus.
                 </p>
               </div>
               <div className="p-6 pt-0">
                 <button
                   className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
                   type="button"
-                  onClick={() => navigate("/doctor")}
+                  onClick={() => navigate("/staff")}
                 >
                   See Doctors List
                 </button>
