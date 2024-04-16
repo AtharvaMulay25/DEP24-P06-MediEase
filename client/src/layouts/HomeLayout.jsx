938f9397b87e97a67e115ecc5d0695f0322ac031
Aryaman Gupta <aryamang2002@gmail.com>
2024-04-17 03:00:55 +0530

    Footer update

--- client/src/layouts/HomeLayout.jsx
+++ client/src/layouts/HomeLayout.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-scroll";
-import { Typography } from "@material-tailwind/react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import Contact from "../models/Contact";
 import { useLogout } from "../hooks/useLogout";
+import Footer from "../components/Footer";
 import { toast } from "sonner";
 
 const HomeLayout = ({ children }) => {
@@ -41,31 +41,6 @@ const HomeLayout = ({ children }) => {
       element.scrollIntoView({ behavior: "smooth" });
     }
   };
-
-  const LINKS = [
-    {
-      title: "",
-      items: ["", "", ""],
-    },
-    {
-      title: "",
-      items: ["", "", ""],
-    },
-    {
-      title: "Services",
-      items: ["Patient", "Medical Staff", "Admin"],
-    },
-    {
-      title: "Contact us",
-      items: [
-        "Medical Center, IIT ROPAR, Punjab - 140001, India",
-        "support@care.com,", //Yet to get one better one
-        "+91-1881-242124, +91-1881-242279",
-        "https://maps.app.goo.gl/dNnoBpedyywiuX4b8",
-      ],
-    },
-  ];
-  const currentYear = new Date().getFullYear();
 
   const [isLargeScreen, setIsLargeScreen] = useState(
     window.matchMedia("(min-width: 890px)").matches
@@ -332,63 +307,7 @@ const HomeLayout = ({ children }) => {
       {/* Content of page */}
       <div className="flex-auto flex flex-col justify-between shadow-lg bg-gray-50 h-screen overflow-y-auto">
         {children}
-        <footer className="w-full mt-5 bg-white">
-          <div className="mx-auto w-full max-w-7xl px-4 py-5">
-            <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
-              <Typography variant="h5" className="mb-6">
-                MediEase
-              </Typography>
-              <div className="grid grid-cols-4 justify-between gap-4">
-                {LINKS.map(({ title, items }) => (
-                  <ul key={title}>
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="mb-1 font-medium opacity-40"
-                    >
-                      {title}
-                    </Typography>
-                    {items.map((link, index) => (
-                      <li key={index}>
-                        {index === items.length - 1 &&
-                        link.startsWith("https://") ? (
-                          <Typography
-                            as="a"
-                            href={link}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            color="gray"
-                            className="py-0.5 font-normal transition-colors hover:text-blue-gray-900"
-                          >
-                            Location
-                          </Typography>
-                        ) : (
-                          <Typography
-                            color="gray"
-                            className="py-0.5 font-normal"
-                          >
-                            {link}
-                          </Typography>
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                ))}
-              </div>
-            </div>
-
-            <div className="mt-3 flex w-full flex-col items-center justify-center border-t border-blue-gray-50 py-2 md:flex-row md:justify-between">
-              <Typography
-                variant="small"
-                className="mb-0 text-center font-normal text-blue-gray-900 md:mb-0"
-              >
-                &copy; {currentYear}{" "}
-                <a href="https://material-tailwind.com/">MediEase</a>. All
-                Rights Reserved.
-              </Typography>
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </div>
   );
