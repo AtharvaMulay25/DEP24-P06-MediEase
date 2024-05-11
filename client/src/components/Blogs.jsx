a3ed6dff779b4040952ff3c818da9d2a760b2dda
Keshav <keshav04ng@gmail.com>
2024-05-11 12:35:51 +0530

    Fix blog post

--- client/src/components/Blogs.jsx
+++ client/src/components/Blogs.jsx
@@ -16,23 +16,24 @@ const Blogs = () => {
           <h1 className=" text-4xl font-semibold text-center lg:text-start">
             Latest Posts
           </h1>
-          <p className=" mt-2 text-center lg:text-start">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus,
-            quidem.
-          </p>
         </div>
       </div>
       <div className=" my-8">
         <div className=" flex flex-wrap justify-center gap-5">
-          <BlogCard img={img1} headlines="Unraveling the Mysteries of Sleep" />
-          <BlogCard img={img2} headlines="The Heart-Healthy Diet" />
+          <BlogCard 
+            img={img1} 
+            headlines="Unraveling the Mysteries of Sleep" 
+            post="Ever wondered why we sleep? Dive into the fascinating world of sleep science with us."
+          />
+          <BlogCard img={img2} headlines="The Heart-Healthy Diet" post="Love your heart with the right foods. Discover the essentials of a heart-healthy diet." />
           <BlogCard
             img={img3}
             headlines="Understanding Pediatric Vaccinations"
+            post="Protecting our little ones. Learn about the importance of pediatric vaccinations."
           />
-          <BlogCard img={img4} headlines="Navigating Mental Health" />
-          <BlogCard img={img5} headlines="The Importance of Regular Exercise" />
-          <BlogCard img={img6} headlines="Skin Health 101" />
+          <BlogCard img={img4} headlines="Navigating Mental Health" post="Mental health matters. Explore insights and support for your well-being."/>
+          <BlogCard img={img5} headlines="The Importance of Regular Exercise" post="Move for health. Discover the benefits of staying active."/>
+          <BlogCard img={img6} headlines="Skin Health 101" post="Love your skin. Learn how to care for it and keep it glowing."/>
         </div>
       </div>
     </div>
--- client/src/layouts/BlogCard.jsx
+++ client/src/layouts/BlogCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BlogCard = ({ img, headlines }) => {
+const BlogCard = ({ img, headlines, post }) => {
   return (
     <div className=" w-full lg:w-1/4 p-2 shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] space-y-2 rounded-lg cursor-pointer hover:scale-105 transition duration-300 ease-in-out bg-white">
       <img
@@ -10,8 +10,7 @@ const BlogCard = ({ img, headlines }) => {
       />
       <h2 className=" text-lg text-center font-semibold">{headlines}</h2>
       <p className=" text-center text-sm">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
-        repellendus suscipit. Rerum consequatur magni expedita.
+        {post}
       </p>
     </div>
   );
